d3d802f66bf4e091c020e8b4fd6f46f5e4229923
Felix Dausch <felix.dausch@plentymarkets.com>
2019-11-06 15:56:13 +0100

    Refactor base widget

--- src/Widgets/Common/CodeWidget.php
+++ src/Widgets/Common/CodeWidget.php
@@ -7,4 +7,27 @@ use Ceres\Widgets\Helper\BaseWidget;
 class CodeWidget extends BaseWidget
 {
     protected $template = "Ceres::Widgets.Common.CodeWidget";
+
+    public function getData()
+    {
+        return [
+            "identifier" => "Ceres::CodeWidget",
+            "label" => "Widget.codeLabel",
+            "previewImageURL" => "/images/widgets/code.svg",
+            "type" => "static",
+            "categories" => ["text"],
+            "position" => 800,
+            "settings" => [
+                "customClass" => [
+                    "type" => "text",
+                    "required" => false,
+                    "defaultValue" => "",
+                    "options" => [
+                        "name" => "Widget.customClass",
+                        "tooltipText" => "Widget.customClassTooltip"
+                    ]
+                ]
+            ]
+        ];
+    }
 }
